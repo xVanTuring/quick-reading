e03c72a5adc954697c72842839bb46ac358beeca
xVanTuring <xVanTuring@foxmail.com>
2023-11-18 16:32:53 +0000

    remove server_files.ts

--- src/serve_file.ts
+++
@@ -1,9 +0,0 @@
-import { staticPlugin } from '@elysiajs/static'
-import Elysia from 'elysia';
-
-export function serveFile(app: Elysia, pathDir: string) {
-    app.use(staticPlugin({
-        assets: pathDir,
-        // prefix: "/book-file",
-    }))
-} 
