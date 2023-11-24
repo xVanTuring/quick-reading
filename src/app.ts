9bd8ebcf1bc0f0cb6ae39d4abd4f2a573b1321c3
xVanTuring <xVanTuring@foxmail.com>
2023-11-24 07:50:35 +0000

    doc: swagger config

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,33 @@
 import { Elysia } from "elysia";
 import { buildApi } from "./api";
+import { swagger } from '@elysiajs/swagger'
 
 export async function buildApp() {
     const app = new Elysia();
     const api = await buildApi();
-    return app.use(api);
+    return app.use(swagger({
+        documentation: {
+            components: {
+                securitySchemes: {
+                    BearerAuth: {
+                        type: "http",
+                        scheme: "bearer",
+                    }
+                }
+            },
+            security: [
+                {
+                    BearerAuth: []
+                }
+            ],
+            info: {
+                title: "QuickReading",
+                version: "1.0.0",
+            }
+        },
+        swaggerOptions: {
+            persistAuthorization: true,
+        }
+    })).use(api);
 }
 
